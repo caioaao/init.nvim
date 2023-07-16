8ce35f6acafc9e6745e5afd42ba4199d2daa4791
Caio Oliveira <caioaao@gmail.com>
2023-07-15 22:22:03 -0300

    add open float keybind

--- lua/me/remap.lua
+++ lua/me/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("i", "<C-;>", "<C-x><C-o>", { remap = true })
 vim.keymap.set("n", ",<Space>", ":nohlsearch<CR>", { silent = true })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
