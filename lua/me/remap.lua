3bc4990ca8d8265b2a8c71464c84de629cbf92f8
Caio Oliveira <caioaao@gmail.com>
2023-07-18 12:15:30 -0300

    use system clipboard

--- lua/me/remap.lua
+++ lua/me/remap.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("i", "<C-;>", "<C-x><C-o>", { remap = true })
 vim.keymap.set("n", ",<Space>", ":nohlsearch<CR>", { silent = true })
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+
+vim.api.nvim_set_option("clipboard", "unnamedplus")
